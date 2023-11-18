31a948529b5aad7a610ecde8bfaa42df282575b8
Xander <xandergott03@gmail.com>
2023-11-18 14:57:24 -0500

    efbwe
    wdifuh

--- popup.js
+++ popup.js
@@ -1,29 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const imageUrlInput = document.getElementById('imageUrl');
-    const speedInput = document.getElementById('speed');
-    const sizeInput = document.getElementById('size');
-    const toggleButton = document.getElementById('toggleBounce');
+document.getElementById('toggleBounce').addEventListener('click', () => {
+  const imageUrl = document.getElementById('imageUrl').value;
+  const speed = document.getElementById('speed').value;
+  const size = document.getElementById('size').value;
 
-    imageUrlInput.addEventListener('input', updateSettings);
-    speedInput.addEventListener('input', updateSettings);
-    sizeInput.addEventListener('input', updateSettings);
-    toggleButton.addEventListener('click', toggleBounce);
-
-    function updateSettings() {
-        sendMessage({
-            imageUrl: imageUrlInput.value,
-            speed: parseInt(speedInput.value, 10),
-            size: parseInt(sizeInput.value, 10)
-        });
-    }
-
-    function toggleBounce() {
-        sendMessage({ toggle: true });
-    }
-
-    function sendMessage(data) {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, data);
-        });
-    }
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      toggle: true,
+      imageUrl: imageUrl,
+      speed: parseInt(speed),
+      size: parseInt(size)
+    });
+  });
 });
