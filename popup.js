6f273f796ae9e2873d247beb77ba40e66d4d5cc0
Bendiestel <bendiestel@me.com>
2023-11-18 17:28:24 -0500

    uh oh

--- popup.js
+++ popup.js
@@ -2,17 +2,14 @@ document.getElementById('toggleBounce').addEventListener('click', () => {
   const imageUrl = document.getElementById('imageUrl').value;
   const speed = document.getElementById('speed').value;
   const size = document.getElementById('size').value;
+  const insanity = document.getElementById('insanityCheckbox').value;
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {
       toggle: true,
       imageUrl: imageUrl,
       speed: parseInt(speed),
-      size: parseInt(size)
+      size: parseInt(size),
+      insanity: insanity
     });
   });
-});
-document.getElementById('insanityButton').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "insanity"});
-    });
 });
