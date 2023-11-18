8d987200db1defeedee79fe3faa3de10b9febe60
Bendiestel <bendiestel@me.com>
2023-11-18 15:04:31 -0500

    eigh3
    iug

--- bounce.js
+++ bounce.js
@@ -1,74 +1,63 @@
 let img = null;
-let dx = 5;
-let dy = 5;
+let dx = 0;
+let dy = 0;
 let x = 0;
 let y = 0;
 let bouncing = false;
 let interval = null;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.toggle) {
-        if (!bouncing) {
-            createBouncingImage(request.imageUrl, request.speed, request.size);
-        } else {
-            removeBouncingImage();
-        }
+  if (request.toggle) {
+    if (!bouncing) {
+      createBouncingImage(request.imageUrl, request.speed, request.size);
     } else {
-        if (bouncing) {
-            updateBouncingImage(request.imageUrl, request.speed, request.size);
-        }
+      removeBouncingImage();
     }
+  }
 });
 
 function createBouncingImage(src, speed, sizePercentage) {
-    img = document.createElement('img');
-    img.src = src;
-    img.style.position = 'fixed';
-    img.style.left = '0px';
-    img.style.top = '0px';
-    img.style.zIndex = 1000;
-    updateImageProperties(speed, sizePercentage);
-    document.body.appendChild(img);
-    bouncing = true;
-    interval = setInterval(updatePosition, 10);
-}
+  if (img) return;
 
-function updateBouncingImage(src, speed, sizePercentage) {
-    img.src = src;
-    updateImageProperties(speed, sizePercentage);
-}
+  img = document.createElement('img');
+  img.src = src;
+  img.style.position = 'fixed';
+  img.style.left = '0px';
+  img.style.top = '0px';
+  img.style.zIndex = 1000;
+  img.style.width = `${sizePercentage}%`;
+  document.body.appendChild(img);
 
-function updateImageProperties(speed, sizePercentage) {
-    const imageSize = (sizePercentage / 100) * window.innerWidth;
-    img.style.width = `${sizePercentage}%`;
-    dx = speed;
-    dy = speed;
-    x = Math.min(x, window.innerWidth - imageSize);
-    y = Math.min(y, window.innerHeight - imageSize);
-}
+  const imageSize = (sizePercentage / 100) * window.innerWidth;
+  dx = speed;
+  dy = speed;
+  x = 0;
+  y = 0;
+  bouncing = true;
+  interval = setInterval(updatePosition, 10);
 
-function updatePosition() {
+  function updatePosition() {
     x += dx;
     y += dy;
 
-    const imgWidth = img.offsetWidth;
-    const imgHeight = img.offsetHeight;
-
-    if (x <= 0 || x >= window.innerWidth - imgWidth) {
-        dx = -dx;
+    if (x <= 0 || x >= window.innerWidth - imageSize) {
+      dx = -dx;
     }
 
-    if (y <= 0 || y >= window.innerHeight - imgHeight) {
-        dy = -dy;
+    if (y <= 0 || y >= window.innerHeight - imageSize) {
+      dy = -dy;
     }
 
     img.style.left = x + 'px';
     img.style.top = y + 'px';
+  }
 }
 
 function removeBouncingImage() {
-    clearInterval(interval);
-    document.body.removeChild(img);
-    img = null;
-    bouncing = false;
+  if (!img) return;
+
+  clearInterval(interval);
+  document.body.removeChild(img);
+  img = null;
+  bouncing = false;
 }
