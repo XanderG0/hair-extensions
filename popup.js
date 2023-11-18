c8fd878d225d8659d5db7f4f566b5a2c8e36aee5
Bendiestel <bendiestel@me.com>
2023-11-18 15:12:16 -0500

    CSS UPDATE
    CSS UPDATE

--- popup.js
+++ popup.js
@@ -1,14 +1,14 @@
 document.getElementById('toggleBounce').addEventListener('click', () => {
-    const imageUrl = document.getElementById('imageUrl').value;
-    const speed = document.getElementById('speed').value;
-    const size = document.getElementById('size').value;
-  
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        toggle: true,
-        imageUrl: imageUrl,
-        speed: parseInt(speed),
-        size: parseInt(size)
-      });
+  const imageUrl = document.getElementById('imageUrl').value;
+  const speed = document.getElementById('speed').value;
+  const size = document.getElementById('size').value;
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      toggle: true,
+      imageUrl: imageUrl,
+      speed: parseInt(speed),
+      size: parseInt(size)
     });
   });
+});
