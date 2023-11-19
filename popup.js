7f2c6461acf38fde922f1c8ff501d5d1bdf709ca
Xander <xandergott03@gmail.com>
2023-11-18 19:00:46 -0500

    Removed functionality
    Working version with attempted extra functionality removed

--- popup.js
+++ popup.js
@@ -2,14 +2,13 @@ document.getElementById('toggleBounce').addEventListener('click', () => {
   const imageUrl = document.getElementById('imageUrl').value;
   const speed = document.getElementById('speed').value;
   const size = document.getElementById('size').value;
-  const insanity = document.getElementById('insanityCheckbox').value;
+
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {
       toggle: true,
       imageUrl: imageUrl,
       speed: parseInt(speed),
-      size: parseInt(size),
-      insanity: insanity
+      size: parseInt(size)
     });
   });
 });
